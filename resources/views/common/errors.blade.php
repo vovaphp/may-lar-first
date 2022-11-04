368d8626d09d7757cf1f03635e5ad4d806be26bc
vovaphp <sahno023@gmail.com>
2022-11-04 17:25:16 +0200

    view common.errors add errors foreach

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,1 +1,14 @@
-<?php
+@if (count($errors) > 0)
+    <!-- Список ошибок формы -->
+    <div class="alert alert-danger">
+        <strong>Упс! Что-то пошло не так!</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
